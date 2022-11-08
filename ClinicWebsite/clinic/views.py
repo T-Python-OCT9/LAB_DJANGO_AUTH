3295807a1ef11081d3eb4b2dd8c52cd8df8506d6
Abdulmohsen Alateeq <mohsenalateeq@gmail.com>
2022-11-08 07:12:35 +0300

    Finalize with authorization groups Managers and Patients.

--- ClinicWebsite/clinic/views.py
+++ ClinicWebsite/clinic/views.py
@@ -3,33 +3,50 @@ from django.http import HttpRequest
 from .models import Doctor, Appointment
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group, User
 
 # Create your views here.
 def homePage(request: HttpRequest):
     doctors = Doctor.objects.all()[:4]
-    context = {'doctors':doctors}
+    if request.user.has_perm('clinic.delete_appointment'):
+        group = 'admin'
+    else:
+        group = 'user'
+    context = {'doctors':doctors, 'group':group}
     return render(request, 'clinic/home.html', context)
 
 @login_required()
 def getDoctors(request: HttpRequest):
     doctors = Doctor.objects.all()
+    if request.user.has_perm('clinic.delete_appointment'):
+        group = 'admin'
+    else:
+        group = 'user'
     for doctor in doctors:
         doctor.appointments = Appointment.objects.filter(doctor=doctor).count
-    context = {'doctors':doctors}
+    context = {'doctors':doctors, 'group':group}
     return render(request, 'clinic/list.html', context)
 
 @login_required()
 def addDoctor(request: HttpRequest):
     doctor_specializations = Doctor.specialization_choices.choices
+    if request.user.has_perm('clinic.delete_appointment'):
+        group = 'admin'
+    else:
+        group = 'user'
     if request.method == 'POST':
         doctor = Doctor(name=request.POST['name'], description=request.POST['description'], experience_years=request.POST['experience_years'], rating=request.POST['rating'], specialization=request.POST['specialization'], image=request.FILES['image'])
         doctor.save()
         return redirect('clinic:doctors')
-    context = {'doctor_specializations':doctor_specializations}
+    context = {'doctor_specializations':doctor_specializations, 'group':group}
     return render(request, 'clinic/add.html', context)
 
 @login_required()
 def updateDoctor(request: HttpRequest, doctor_id: int):
+    if request.user.has_perm('clinic.delete_appointment'):
+        group = 'admin'
+    else:
+        group = 'user'
     try:
         doctor = Doctor.objects.get(id=doctor_id)
         doctor_specializations = Doctor.specialization_choices.choices
@@ -45,7 +62,7 @@ def updateDoctor(request: HttpRequest, doctor_id: int):
             return redirect('clinic:doctors')
     except:
         return render(request, 'clinic/not_found.html')
-    context = {'doctor_specializations':doctor_specializations, 'doctor':doctor}
+    context = {'doctor_specializations':doctor_specializations, 'doctor':doctor, 'group':group}
     return render(request, 'clinic/update.html', context)
 
 @login_required()
@@ -61,12 +78,17 @@ def removeDoctor(request: HttpRequest, doctor_id: int):
 def doctorDetail(request: HttpRequest, doctor_id: int):
     try:
         doctor = Doctor.objects.get(id=doctor_id)
-        appointments = Appointment.objects.filter(doctor=doctor)
+        if request.user.has_perm('clinic.delete_appointment'):
+            appointments = Appointment.objects.filter(doctor=doctor)
+            group = 'admin'
+        else:
+            appointments = Appointment.objects.filter(doctor=doctor, name=request.user.username)
+            group = 'user'
         count = appointments.count
         date = datetime.isoformat(datetime.today(), timespec='minutes').replace('+',':')
     except:
         return render(request, 'clinic/not_found.html')
-    context = {'doctor':doctor, 'appointments':appointments, 'count':count, 'date':date}
+    context = {'doctor':doctor, 'appointments':appointments, 'count':count, 'date':date, 'group':group}
     return render(request, 'clinic/detail.html', context)
 
 @login_required()
@@ -74,18 +96,27 @@ def bookAppointment(request: HttpRequest, doctor_id: int):
     try:
         doctor = Doctor.objects.get(id=doctor_id)
         if request.method == 'POST':
-            new_appointment = Appointment(doctor=doctor, name=request.POST['name'], case=request.POST['case'], age=request.POST['age'])
+            print(request.POST)
+            new_appointment = Appointment(doctor=doctor, name=request.POST['name'], case=request.POST['case'], age=request.POST['age'], date=request.POST['date'].replace(':','+'))
             new_appointment.save()
     except:
         return render(request, 'clinic/not_found.html')
     return redirect('clinic:detail', doctor.id)
 
 def searchDoctor(request: HttpRequest):
+    if request.user.has_perm('clinic.delete_appointment'):
+        group = 'admin'
+    else:
+        group = 'user'
     doctors = Doctor.objects.all().filter(name__contains=request.GET.get('doctor', ''))
-    context = {'doctors':doctors}
+    context = {'doctors':doctors, 'group':group}
     return render(request, 'clinic/search.html', context)
 
 def galleryPage(request: HttpRequest):
-    context = {}
+    if request.user.has_perm('clinic.delete_appointment'):
+        group = 'admin'
+    else:
+        group = 'user'
+    context = {'group':group}
     return render(request, 'clinic/gallery.html', context)
 
